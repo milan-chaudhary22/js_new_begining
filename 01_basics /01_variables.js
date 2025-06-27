d645923d84f8026bd78ad7da89e8f2e35dc75b95
milan-chaudhary22 <milanchaudharym41@gmail.com>
2025-06-27 09:31:25 +0000

    new configruation commited

---
+++ 01_basics /01_variables.js
@@ -0,0 +1,11 @@
+
+const  accountNumber = 25455
+let accountEmail = "example.com"
+var accountId = 12454212
+
+Name = "Milan Chaudhary"
+
+accountEmail = "xyzmilan.com"
+accountId = 45161152
+
+ console.table
